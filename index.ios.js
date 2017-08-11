0c57bae6cf6aac9cebbdfe024cdc9b2f8d65cd71
George Paul <george.allen.paul.3@gmail.com>
2017-08-11 15:49:58 -0400

    Add scroll enabled state in parent when callout is called

--- index.ios.js
+++ index.ios.js
@@ -41,8 +41,14 @@ export default class reactNativeParalax extends Component {
   constructor () {
     super()
     this.state = {
-      animatedScroll: new Animated.Value(0)
+      animatedScroll: new Animated.Value(0),
+      scrollEnabled: true
     }
+    this.handleFocus = this.handleFocus.bind(this)
+  }
+
+  handleFocus (focused) {
+    this.setState({ scrollEnabled: !focused })
   }
 
   render () {
@@ -51,6 +57,7 @@ export default class reactNativeParalax extends Component {
         <ScrollView
           pagingEnabled
           horizontal
+          scrollEnabled={this.state.scrollEnabled}
           scrollEventThrottle={16}
           onScroll={
             Animated.event([
@@ -69,6 +76,8 @@ export default class reactNativeParalax extends Component {
               key={i}
               {...image}
               translateX={getInterpolate(this.state.animatedScroll, i, Images.length)}
+              onFocus={this.handleFocus}
+              focused={!this.state.scrollEnabled}
             />
           ))}
           {Array.apply(null, { length: Images.length + 1 }).map((_, i) => getSeparator(i))}
